4ab5403fda62afb71cfcfe4d6be75fcee32c9f09
Nathan Cox <jncox@users.noreply.github.com>
2022-06-15 09:34:16 -0700

    Updates for Markdown

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,8 @@ extensions = ['sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinxcontrib.fulltoc',
     'sphinx_copybutton',
-    'sphinx_fontawesome']
+    'sphinx_fontawesome',
+    'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +48,14 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+#source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
